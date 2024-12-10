77507524e4a7ef053dca0c7cc30b16c5c116da14
Chukwuebuka Chidi-Ezirim <chizim2003@gmail.com>
2024-12-10 09:19:13 +0100

    created a diary list component

--- src/components/DiaryList.tsx
+++ src/components/DiaryList.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { Entry } from "../utils/diary";
-import lin
+
 
 interface EntryListProps {
-    entries: Entry[];
-    deleteEntry: (id: string) => void;
-  }
-const DiaryList: React.FC<EntryListProps> =({entries, deleteEntry}) =>{
-return(
-    <section>
-
-    </section>
-)
+  entries: Entry[];
+  deleteEntry: (id: string) => void;
 }
+const DiaryList: React.FC<EntryListProps> = ({ entries, deleteEntry }) => {
+  return (
+    <section>
+      {entries.map((entry) => (
+        <div key={entry.id}>
+          <h3>{entry.title}</h3>
+          <h4>{entry.date}</h4>
+          <p>{entry.body}</p>
+          <button onClick={() => deleteEntry}>Delete</button>
+        </div>
+      ))}
+    </section>
+  );
+};
 export default DiaryList;

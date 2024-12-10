762e0b825f27ee392cdfff89cfd86ff124bf4bfd
Chukwuebuka Chidi-Ezirim <chizim2003@gmail.com>
2024-12-10 09:26:43 +0100

    added a link component to the diary details component from the diary list

--- src/components/DiaryList.tsx
+++ src/components/DiaryList.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Entry } from "../utils/diary";
-
+import {Link} from "react-router-dom"
 
 interface EntryListProps {
   entries: Entry[];
@@ -13,8 +13,8 @@ const DiaryList: React.FC<EntryListProps> = ({ entries, deleteEntry }) => {
         <div key={entry.id}>
           <h3>{entry.title}</h3>
           <h4>{entry.date}</h4>
-          <p>{entry.body}</p>
           <button onClick={() => deleteEntry}>Delete</button>
+          <Link to ={`DiaryDetails${entry.date}`} state ={{entry}}>Read More</Link>
         </div>
       ))}
     </section>

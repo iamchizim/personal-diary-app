83c6a5747c44fbc4a6322f839fb869813f2c6e6f
Chukwuebuka Chidi-Ezirim <chizim2003@gmail.com>
2024-12-10 09:31:29 +0100

    created a diary details component

---
+++ src/components/DiaryDetails.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+const DiaryDetails = () =>{
+
+}
+export default DiaryDetails;
--- src/components/DiaryList.tsx
+++ src/components/DiaryList.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Entry } from "../utils/diary";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 interface EntryListProps {
   entries: Entry[];
@@ -14,7 +14,9 @@ const DiaryList: React.FC<EntryListProps> = ({ entries, deleteEntry }) => {
           <h3>{entry.title}</h3>
           <h4>{entry.date}</h4>
           <button onClick={() => deleteEntry}>Delete</button>
-          <Link to ={`DiaryDetails${entry.date}`} state ={{entry}}>Read More</Link>
+          <Link to={`DiaryDetails${entry.date}`} state={{ entry }}>
+            Read More
+          </Link>
         </div>
       ))}
     </section>
